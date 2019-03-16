08b8a3b1c5623d55bf6826193f1058a21a85f65a
keshavmandayam <keshavmandayam@gmail.com>
2019-03-15 20:51:38 -0700

    Added input accuracy logic check to display progress accordingly

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import Challenge from './challenge.jsx';
 import InputForm from './inputForm.jsx';
+import DisplayProgress from './displayProgress.jsx';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       challenge: 'These are words that you would need to type.',
-      typedValue: ''
+      typedValue: '',
+      displayValue: []
     };
     this.handleInput = this.handleInput.bind(this);
   }
 
   handleInput(event) {
+    this.checkAccuracy(event.target.value);
     let newState = this.state;
     newState.typedValue = event.target.value;
+    this.setState(newState);
+  }
+  checkAccuracy(string) {
+    const displayArr = [];
+    for (let i = 0; i < string.length; i++) {
+      string[i] === this.state.challenge[i]
+        ? displayArr.push([string[i], true])
+        : displayArr.push([string[i], false]);
+    }
+    let newState = this.state;
+    newState.displayValue = displayArr;
     this.setState(newState);
   }
 
@@ -22,6 +36,7 @@ class App extends Component {
     return (
       <div>
         <Challenge challenge={this.state.challenge} />
+        <DisplayProgress text={this.state.displayValue} />
         <InputForm
           value={this.state.typedValue}
           handleInput={this.handleInput}
